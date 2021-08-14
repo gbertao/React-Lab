15908d1d08e394d46e2d9c13934e19864dab5cea
Giovanni <gbertao96@gmail.com>
2021-08-14 15:21:27 +0000

    onDelete: implementation

--- tracker-app/src/App.js
+++ tracker-app/src/App.js
@@ -26,13 +26,17 @@ function App() {
 
   // Delete Task
   const deleteTask = (id) => {
-    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   return (
     <div className="container">
-	  <Header />
-	  <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+      ) : (
+        'No Tasks to show'
+      )}
     </div>
   );
 }
