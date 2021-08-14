5e63163d51449e3ef21aa307606fba190fb1c17f
Giovanni <gbertao96@gmail.com>
2021-08-14 15:19:21 +0000

    onDelete function

--- tracker-app/src/components/Task.js
+++ tracker-app/src/components/Task.js
@@ -1,9 +1,9 @@
 import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className='task'>
-      <h3>{task.name} <FaTimes style={{ color:'red', cursor: 'pointer' }}/></h3>
+      <h3>{task.name} <FaTimes onClick={() => onDelete(task.id)} style={{ color:'red', cursor: 'pointer' }}/></h3>
       <p>{task.day}</p>
     </div>
   )
