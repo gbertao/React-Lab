85e980f4e2afcf6ee6d2aef9bc7adeacb5f568d7
Giovanni <gbertao96@gmail.com>
2021-08-14 16:41:21 +0000

    addTask: get form's info and append to the task list

--- tracker-app/src/App.js
+++ tracker-app/src/App.js
@@ -7,23 +7,33 @@ function App() {
   const [tasks, setTasks] = useState([
     {
       id:1,
-      name: 'Giovanni',
+      text: 'Giovanni',
       day: '01/01/01',
       reminder: true,
     },
     {
       id:2,
-      name: 'Gi',
+      text: 'Gi',
       day: '02/02/02',
       reminder: true,
     },
     {
       id:3,
-      name: 'Gio',
+      text: 'Gio',
       day: '03/03/03',
       reminder: false,
     },
   ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+
+    const newTask = { id, ...task }
+
+    setTasks([...tasks, newTask])
+    console.log(tasks)
+  }
 
   // Toggle Reminder
   const toggleReminder = (id) => {
@@ -42,7 +52,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask}/>
       ) : (
