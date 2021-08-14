66cf1f7e65fffb096feafb94330094202bd288e6
Giovanni <gbertao96@gmail.com>
2021-08-14 15:53:56 +0000

    toggle reminder: change Task card

--- tracker-app/src/App.js
+++ tracker-app/src/App.js
@@ -24,6 +24,15 @@ function App() {
     },
   ])
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map(
+        (task) => task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    )
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -33,7 +42,7 @@ function App() {
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        <Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask}/>
       ) : (
         'No Tasks to show'
       )}
